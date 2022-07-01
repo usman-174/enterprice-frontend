758cc605b40ac9313dfb250249a61a6d0349b21c
usman-174 <hellmughal123@gmail.com>
2022-07-01 22:23:19 +0500

    dawd

--- src/components/dashboard/AddDirector.jsx
+++ src/components/dashboard/AddDirector.jsx
@@ -39,7 +39,7 @@ const AddDirector = ({
   const [username, setUsername] = React.useState("");
   const [manageList, setManageList] = React.useState([]);
   const [direction, setDirection] = React.useState("");
-  const [directions, setDirections] = React.useState(allDirections || []);
+  const [directions, setDirections] = React.useState(allDirections);
   console.log({directions,allDirections});
   const [department, setDepartment] = React.useState("");
   const [departments, setDepartments] = React.useState([]);
@@ -104,9 +104,12 @@ const AddDirector = ({
     }
   };
   useEffect(() => {
+    if(allDirections){
+
       setDirections(allDirections);
+    }
     // eslint-disable-next-line
-  }, []);
+  }, [allDirections]);
   return (
     <Box sx={{ position: "relative" }}>
       <Button
